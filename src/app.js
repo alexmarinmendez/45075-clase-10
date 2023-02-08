632159df40249a6855a2ef55f9bede73b4ea28b1
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-02-08 18:53:50 -0500

    feat: actividad de clase

--- src/app.js
+++ src/app.js
@@ -11,10 +11,16 @@ app.set('view engine', 'handlebars')
 app.listen(8080, () => console.log('Server Up'))
 
 app.get('/', (req, res) => {
-    res.render('index', {
-        title: 'Página princial',
-        nombre: 'Piqué'
-    })
+
+    const users = [
+        { nombre: 'Alex', apellido: 'Marin', edad: 45, correo: 'alexmarinmendez@gmail.com' },
+        { nombre: 'Pablo', apellido: 'Volpe', edad: 15, correo: 'pablovolpe@gmail.com' },
+        { nombre: 'Victor', apellido: 'Hernandez', edad: 29, correo: 'vhernandez@gmail.com' },
+        { nombre: 'Emanuel', apellido: 'Cargino', edad: 85, correo: 'emanuelcargino@gmail.com' },
+        { nombre: 'Evelin', apellido: 'Capaijira', edad: 18, correo: 'evelin@gmail.com' },
+    ]
+    const indexAleatorio = Math.floor(Math.random() * 5)
+    res.render('actividad', users[indexAleatorio])
 })
 
 app.get('/otraRuta', (req, res) => {
