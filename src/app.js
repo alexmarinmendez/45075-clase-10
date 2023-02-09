eb077c10f143e494d75aa8515296a5ab8455a215
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-02-08 19:27:38 -0500

    feat: change routes

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import handlebars from 'express-handlebars'
 import __dirname from './utils.js'
+import viewRouter from './routes/viewRoute.js'
 
 const app = express()
 
@@ -10,31 +11,7 @@ app.set('view engine', 'handlebars')
 
 app.listen(8080, () => console.log('Server Up'))
 
-const foods = [
-    { name: 'Banana', price: 10 },
-    { name: 'Wine', price: 150 },
-    { name: 'Hot Dogs', price: 100 },
-    { name: 'Hamburguesas', price: 110 },
-    { name: 'Beers', price: 75 },
-]
-
-app.get('/', (req, res) => {
-    const users = [
-        { nombre: 'Alex', apellido: 'Marin', edad: 45, correo: 'alexmarinmendez@gmail.com', role: 'user' },
-        { nombre: 'Pablo', apellido: 'Volpe', edad: 15, correo: 'pablovolpe@gmail.com', role: 'admin' },
-        { nombre: 'Victor', apellido: 'Hernandez', edad: 29, correo: 'vhernandez@gmail.com', role: 'user' },
-        { nombre: 'Emanuel', apellido: 'Cargino', edad: 85, correo: 'emanuelcargino@gmail.com', role: 'user' },
-        { nombre: 'Evelin', apellido: 'Capaijira', edad: 18, correo: 'evelin@gmail.com', role: 'user' },
-    ]
-    const indexAleatorio = Math.floor(Math.random() * 5)
-    // users[indexAleatorio].title = `Datos del usuario ${indexAleatorio}`
-    res.render('actividad2', {
-        title: 'Datos del usuario',
-        ...users[indexAleatorio],
-        isAdmin: users[indexAleatorio].role == 'admin',
-        foods
-    })
-})
+app.use('/', viewRouter)
 
 app.get('/otraRuta', (req, res) => {
     res.render('otra', {
---
+++ src/routes/viewRoute.js
@@ -0,0 +1,31 @@
+import express from 'express'
+
+const router = express.Router()
+
+const foods = [
+    { name: 'Banana', price: 10 },
+    { name: 'Wine', price: 150 },
+    { name: 'Hot Dogs', price: 100 },
+    { name: 'Hamburguesas', price: 110 },
+    { name: 'Beers', price: 75 },
+]
+
+router.get('/', (req, res) => {
+    const users = [
+        { nombre: 'Alex', apellido: 'Marin', edad: 45, correo: 'alexmarinmendez@gmail.com', role: 'user' },
+        { nombre: 'Pablo', apellido: 'Volpe', edad: 15, correo: 'pablovolpe@gmail.com', role: 'admin' },
+        { nombre: 'Victor', apellido: 'Hernandez', edad: 29, correo: 'vhernandez@gmail.com', role: 'user' },
+        { nombre: 'Emanuel', apellido: 'Cargino', edad: 85, correo: 'emanuelcargino@gmail.com', role: 'user' },
+        { nombre: 'Evelin', apellido: 'Capaijira', edad: 18, correo: 'evelin@gmail.com', role: 'user' },
+    ]
+    const indexAleatorio = Math.floor(Math.random() * 5)
+    // users[indexAleatorio].title = `Datos del usuario ${indexAleatorio}`
+    res.render('actividad2', {
+        title: 'Datos del usuario',
+        ...users[indexAleatorio],
+        isAdmin: users[indexAleatorio].role == 'admin',
+        foods
+    })
+})
+
+export default router
